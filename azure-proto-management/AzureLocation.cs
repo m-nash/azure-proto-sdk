31ca057a02dee25935ba205c5124c192212beeaf
markcowl <markcowl@microsoft.com>
2020-08-21 19:50:13 -0700

    Remove management package

--- azure-proto-management/AzureLocation.cs
+++
@@ -1,9 +0,0 @@
-﻿using azure_proto_core;
-
-namespace azure_proto_management
-{
-    public class AzureLocation: AzureOperations
-    {
-        public AzureLocation(AzureSubscription subscription, PhLocation location) { Id = location.Id; Location = location.Name; }
-    }
-}
