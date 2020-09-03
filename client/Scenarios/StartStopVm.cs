ffd129701bd75ff8ffa22cfa4d97248574094e7f
m-nash <64171366+m-nash@users.noreply.github.com>
2020-09-03 13:35:54 -0700

    remove changes to startstopvm scenario

--- client/Scenarios/StartStopVm.cs
+++ client/Scenarios/StartStopVm.cs
@@ -13,27 +13,6 @@ namespace client
 
             var client = new ArmClient();
             var subscription = client.Subscriptions(Context.SubscriptionId);
-
-
-            //subscription.ResourceGroups() --Container<T> //allows for Create/Delete methods builder overloads
-            //subscription.ListResourceGroups() --Pageable<Entity<T>> //sync/async has filter overloads
-            //foreach(var rg in subscription.ListResourceGroups()) { //do stuff }
-            //subscription.ResourceGroup(rgName) --Entity<T> //no network call so has empty Model with Operations
-            //subscription.ListVirtualMachines() --Pageable<Entity<T>>
-            //subscription.ResourceGroup(rgName).ListVirtualMachines() --Pageable<Entity<T>>
-
-            //recommend if we want common functionality in addition to what Pageable<T> provides we create an ArmPageable<T> to add that functionality
-            //this removes confusion on collection vs container but maintains the majority of that separation
-
-            //subscription.ResourceGroups().List() --Pagabel<Entity<T>>
-            //subscription.ListNics() --Pageabel<Entity<T>>
-            //subscription.ResourceGroup(rgName).ListNics() --Pageabel<Entity<T>>
-
-            //armClient.ListNics() --Pageabel<Entity<T>>
-            //armClient.DefaultSubscription.ResourceGroups() --Container<T>
-            //armClient.Subscription(subId) --Entity<T>
-
-
             var resourceGroup = subscription.ResourceGroup(Context.RgName);
             var vm = resourceGroup.Vm(Context.VmName);
             Console.WriteLine($"Found VM {Context.VmName}");
