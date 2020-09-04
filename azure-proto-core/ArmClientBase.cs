8bb35d1c5e055e757087b60497f1fa56e8a7b737
markcowl <markcowl@microsoft.com>
2020-09-04 12:16:33 -0700

    Minor cleanup from merge

--- azure-proto-core/ArmClientBase.cs
+++ azure-proto-core/ArmClientBase.cs
@@ -20,12 +20,12 @@ namespace azure_proto_core
         {
         }
 
-        //public T GetOperations<T>(ResourceIdentifier context) where T : ResourceOperations
+        //public T GetOperations<T>(ResourceIdentifier context) where T : ResourceClientBase
         //{
         //    return Activator.CreateInstance(typeof(T), this, context) as T;
         //}
 
-        //public T GetOperations<T>(azure_proto_core.Resource context) where T : ResourceOperations
+        //public T GetOperations<T>(azure_proto_core.Resource context) where T : ResourceClientBase
         //{
         //    return Activator.CreateInstance(typeof(T), this, context) as T;
         //}
@@ -36,6 +36,13 @@ namespace azure_proto_core
         protected Uri BaseUri { get; }
 
         protected abstract ResourceType ResourceType { get; }
+
+        /// <summary>
+        /// Note that this is currently adapting to underlying management clients - once generator changes are in, this would likely be unnecessary
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="creator"></param>
+        /// <returns></returns>
         protected T GetClient<T>(Func<Uri, TokenCredential, T> creator)
         {
             return creator(BaseUri, Credential);
--- azure-proto-core/ResourceOperationsBase.cs
+++ azure-proto-core/ResourceOperationsBase.cs
@@ -1,7 +1,4 @@
-﻿using Azure;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace azure_proto_core
 {
@@ -42,73 +39,6 @@ namespace azure_proto_core
                 throw new InvalidOperationException($"{identifier} is not a valid resource of type {ResourceType}");
             }
         }
-    }
-
-
-    /// <summary>
-    /// Common base type for lifecycle operations over a resource
-    /// TODO: Consider whehter to represent POST operations and the acommpanyong actions list call
-    /// TODO: Consider whether to provide a Normalized PATCH functionality across RP resources
-    /// TODO: Refactor methods beyond the ResourceOperation as extensions [allowing them to appear in generic usage of the type]
-    /// </summary>
-    /// <typeparam name="Model"></typeparam>
-    public abstract class ResourceClientBase<T> : ResourceOperationsBase where T : Resource 
-    {
-        public ResourceClientBase(ArmClientBase parent, ResourceIdentifier context) : base(parent, context)
-        {
-            Resource = new ArmResource(context);
-        }
-
-        public ResourceClientBase(ArmClientBase parent, Resource context) : base(parent, context)
-        {
-            Resource = context;
-        }
-
-
-        protected override Resource Resource { get;  set; }
-        public override ResourceIdentifier Context => Resource.Id;
-
-        public virtual bool HasModel { 
-            get 
-            {
-                var model = Resource as T;
-                return model != null;
-            }
-        }
-
-        public virtual T Model
-        {
-            get
-            {
-                return Resource as T;
-            }
-        }
-
-        public async virtual Task<T> GetModelIfNewerAsync(CancellationToken cancellationToken = default)
-        {
-            if (HasModel)
-            {
-                return Model;
-            }
-
-            return (await GetAsync(cancellationToken)).Value.Model;
-        }
-
-        public virtual T GetModelIfNewer()
-        {
-            if (HasModel)
-            {
-                return Model;
-            }
-
-            return Get().Value.Model;
-        }
-
-        public abstract Response<ResourceClientBase<T>> Get();
-        public abstract Task<Response<ResourceClientBase<T>>> GetAsync(CancellationToken cancellationToken = default);
-        public abstract ArmOperation<ResourceClientBase<T>> AddTag(string key, string value);
-        public abstract Task<ArmOperation<ResourceClientBase<T>>> AddTagAsync(string key, string value, CancellationToken cancellationToken = default);        public abstract ArmOperation<Response> Delete();
-        public abstract Task<ArmOperation<Response>> DeleteAsync(CancellationToken cancellationToken = default);
     }
 
 }
