dfd594fac87facf0a64e0608a75fdfc333be0729
Jonathan Cardenas <JonathanCrd@users.noreply.github.com>
2020-11-02 11:00:37 -0800

    Test granularity

--- azure-proto-core-test/LocationTests.cs
+++ azure-proto-core-test/LocationTests.cs
@@ -20,17 +20,24 @@ namespace azure_proto_core_test
         }
 
         [Test]
-        public void Can()
+        public void EqualsToTrue()
         {
             Location location1 = new Location("West US");
             Location location2 = new Location("West US");
-            Location location3 = new Location("South US");
 
-            Assert.IsTrue(location1.Equals(location2));
             Assert.IsTrue(location1.Equals(location1));
-            Assert.IsFalse(location1.Equals(location3));
+            Assert.IsTrue(location1.Equals(location2));
 
             Assert.IsTrue(location1.Equals("West US"));
+        }
+
+        [Test]
+        public void EqualsToFalse()
+        {
+            Location location1 = new Location("West US");
+            Location location2 = new Location("South US");
+
+            Assert.IsFalse(location1.Equals(location2));
             Assert.IsFalse(location1.Equals("WestUS"));
         }
 
@@ -43,20 +50,42 @@ namespace azure_proto_core_test
         }
 
         [Test]
-        public void CanCompareTo()
+        public void CanCompareToEquals()
         {
             Location location1 = new Location("South US");
-            Location location2 = new Location("East US");
-            Location location3 = new Location("West US");
 
             Assert.AreEqual(0, location1.CompareTo(location1));
             Assert.AreEqual(0, location1.CompareTo("South US"));
 
+        }
+
+        [Test]
+        public void CanCompareToGreaterThan()
+        {
+            Location location1 = new Location("South US");
+            Location location2 = new Location("East US");
+
             Assert.AreEqual(1, location1.CompareTo(location2));
             Assert.AreEqual(1, location1.CompareTo("East US"));
+        }
+
+        [Test]
+        public void CanCompareToLessThan()
+        {
+            Location location1 = new Location("South US");
+            Location location3 = new Location("West US");
 
             Assert.AreEqual(-1, location1.CompareTo(location3));
             Assert.AreEqual(-1, location1.CompareTo("West US"));
+        }
+
+        [TestCase (true)]
+        public void CanCompareToNull(bool isNull)
+        {
+            string x = null;
+            Assert.AreEqual(1, Location.Default.CompareTo(isNull ? null : Location.Default));
+            Assert.AreEqual(1, Location.Default.CompareTo(x));
+
         }
 
         [Test]
