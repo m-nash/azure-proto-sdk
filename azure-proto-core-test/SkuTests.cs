0ed77452d0e7dbadc2e662020a6f5727323f5051
YalinLi0312 <yall@microsoft.com>
2020-11-18 15:57:32 -0800

    Implement comparison methods in Sku class and add its tests

---
+++ azure-proto-core-test/SkuTests.cs
@@ -0,0 +1,135 @@
+﻿using azure_proto_core;
+using NUnit.Framework;
+
+namespace azure_proto_core_test
+{
+    class SkuTests
+    {
+        [Test]
+        public void CompareToTests()
+        {
+            Sku sku1 = new Sku();
+            Sku sku2 = null;
+
+            sku1.Name = "name";
+            sku1.Family = "family";
+            sku1.Size = "size";
+            sku1.Tier = "tier";
+            Assert.AreEqual(1, sku1.CompareTo(sku2));
+
+            sku2 = new Sku();
+            sku2.Name = "name";
+            sku2.Family = "family";
+            sku2.Size = "size";
+            sku2.Tier = "tier";
+            Assert.AreEqual(0, sku1.CompareTo(sku2));
+
+            sku1.Capacity = 1;
+            Assert.AreEqual(1, sku1.CompareTo(sku2));
+
+            sku2.Capacity = 2;
+            Assert.AreEqual(-1, sku1.CompareTo(sku2));
+
+            sku2.Capacity = 0;
+            Assert.AreEqual(1, sku1.CompareTo(sku2));
+
+            sku2.Capacity = 1;
+            Assert.AreEqual(0, sku1.CompareTo(sku2));
+
+            sku1.Capacity = null;
+            Assert.AreEqual(-1, sku1.CompareTo(sku2));
+
+            sku1.Tier = "tier1";
+            Assert.AreEqual(1, sku1.CompareTo(sku2));
+
+            sku2.Tier = "tier2";
+            Assert.AreEqual(-1, sku1.CompareTo(sku2));
+
+            sku2.Tier = null;
+            Assert.AreEqual(1, sku1.CompareTo(sku2));
+
+            sku1.Size = "size1";
+            Assert.AreEqual(1, sku1.CompareTo(sku2));
+
+            sku2.Size = "size2";
+            Assert.AreEqual(-1, sku1.CompareTo(sku2));
+
+            sku2.Size = null;
+            Assert.AreEqual(1, sku1.CompareTo(sku2));
+
+            sku1.Family = "family1";
+            Assert.AreEqual(1, sku1.CompareTo(sku2));
+
+            sku2.Family = "family2";
+            Assert.AreEqual(-1, sku1.CompareTo(sku2));
+
+            sku2.Family = null;
+            Assert.AreEqual(1, sku1.CompareTo(sku2));
+
+            sku1.Name = "name1";
+            Assert.AreEqual(1, sku1.CompareTo(sku2));
+
+            sku2.Name = "name2";
+            Assert.AreEqual(-1, sku1.CompareTo(sku2));
+
+            sku2.Name = null;
+            Assert.AreEqual(1, sku1.CompareTo(sku2));
+        }
+
+        [Test]
+        public void EqualsTests()
+        {
+            Sku sku1 = new Sku();
+            Sku sku2 = null;
+
+            sku1.Name = "name";
+            sku1.Family = "family";
+            sku1.Size = "size";
+            sku1.Tier = "tier";
+            Assert.IsFalse(sku1.Equals(sku2)); // sku1 != null && sku2 == null
+
+            sku2 = new Sku();
+            sku2.Name = "name";
+            sku2.Family = "family";
+            sku2.Size = "size";
+            sku2.Tier = "tier";
+            Assert.IsTrue(sku1.Equals(sku2));
+
+            sku1.Capacity = 1;
+            Assert.IsFalse(sku1.Equals(sku2));
+
+            sku2.Capacity = 2;
+            Assert.IsFalse(sku1.Equals(sku2));
+
+            sku2.Capacity = 1;
+            Assert.IsTrue(sku1.Equals(sku2));
+
+            sku1.Capacity = null;
+            Assert.IsFalse(sku1.Equals(sku2));
+
+            sku1.Tier = "tier1";
+            Assert.IsFalse(sku1.Equals(sku2));
+
+            sku2.Tier = null;
+            Assert.IsFalse(sku1.Equals(sku2));
+
+            sku1.Size = "size1";
+            Assert.IsFalse(sku1.Equals(sku2));
+
+            sku2.Size = null;
+            Assert.IsFalse(sku1.Equals(sku2));
+
+            sku1.Family = "family1";
+            Assert.IsFalse(sku1.Equals(sku2));
+
+            sku2.Family = null;
+            Assert.IsFalse(sku1.Equals(sku2));
+
+            sku1.Name = "name1";
+            Assert.IsFalse(sku1.Equals(sku2));
+
+            sku2.Name = null;
+            Assert.IsFalse(sku1.Equals(sku2));
+        }
+    }
+}
--- azure-proto-core/Resources/Sku.cs
+++ azure-proto-core/Resources/Sku.cs
@@ -5,7 +5,6 @@ namespace azure_proto_core
 {
     /// <summary>
     /// Representaion of ARM SKU
-    /// TODO: Implement comparison methods and operator overloads
     /// </summary>
     public class Sku : IEquatable<Sku>, IComparable<Sku>
     {
@@ -17,12 +16,48 @@ namespace azure_proto_core
 
         public int CompareTo(Sku other)
         {
-            throw new NotImplementedException();
+            if (other == null) return 1;
+            if (this.Name.CompareTo(other.Name) == 0)
+            {
+                if (this.Family.CompareTo(other.Family) == 0)
+                {
+                    if (this.Size.CompareTo(other.Size) == 0)
+                    {
+                        if (this.Tier.CompareTo(other.Tier) == 0)
+                        {
+                            if (this.Capacity == null && other.Capacity == null) return 0;
+                            if (other.Capacity == null) return 1;
+                            if (this.Capacity == null) return -1;
+                            return this.Capacity.Value.CompareTo(other.Capacity.Value);
+                        }
+                        return this.Tier.CompareTo(other.Tier);
+                    }
+                    return this.Size.CompareTo(other.Size);
+                }
+                return this.Family.CompareTo(other.Family);
+            }
+            return this.Name.CompareTo(other.Name);
         }
 
         public bool Equals(Sku other)
         {
-            throw new NotImplementedException();
+            if (other == null) return false;
+            if (this.Name.Equals(other.Name))
+            {
+                if (this.Family.Equals(other.Family))
+                {
+                    if (this.Size.Equals(other.Size))
+                    {
+                        if (this.Tier.Equals(other.Tier))
+                        {
+                            if (this.Capacity == null && other.Capacity == null) return true;
+                            if (this.Capacity == null || other.Capacity == null) return false;
+                            return this.Capacity.Equals(other.Capacity);
+                        }
+                    }
+                }
+            }
+            return false;
         }
     }
 }
