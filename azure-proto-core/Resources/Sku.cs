6436e3915ae7e9c66f8104c8db06640924e015a2
YalinLi0312 <yall@microsoft.com>
2020-11-20 11:58:33 -0800

    Handle the case when this.property's value is null

--- azure-proto-core/Resources/Sku.cs
+++ azure-proto-core/Resources/Sku.cs
@@ -17,42 +17,43 @@ namespace azure_proto_core
         public int CompareTo(Sku other)
         {
             if (other == null) return 1;
-            if (this.Name.CompareTo(other.Name) == 0)
+            if (this.Name == null && other.Name == null || this.Name != null && this.Name.CompareTo(other.Name) == 0)
             {
-                if (this.Family.CompareTo(other.Family) == 0)
+                if (this.Family == null && other.Family == null || this.Family != null && this.Family.CompareTo(other.Family) == 0)
                 {
-                    if (this.Size.CompareTo(other.Size) == 0)
+                    if (this.Size == null && other.Size == null || this.Size != null && this.Size.CompareTo(other.Size) == 0)
                     {
-                        if (this.Tier.CompareTo(other.Tier) == 0)
+                        if (this.Tier == null && other.Tier == null || this.Tier != null && this.Tier.CompareTo(other.Tier) == 0)
                         {
                             if (this.Capacity == null && other.Capacity == null) return 0;
-                            if (other.Capacity == null) return 1;
                             if (this.Capacity == null) return -1;
+                            if (other.Capacity == null) return 1;
                             return this.Capacity.Value.CompareTo(other.Capacity.Value);
                         }
-                        return this.Tier.CompareTo(other.Tier);
+                        return this.Tier == null ? -1 : this.Tier.CompareTo(other.Tier);
                     }
-                    return this.Size.CompareTo(other.Size);
+                    return this.Size == null ? -1 : this.Size.CompareTo(other.Size);
                 }
-                return this.Family.CompareTo(other.Family);
+                return this.Family == null ? -1 : this.Family.CompareTo(other.Family);
             }
-            return this.Name.CompareTo(other.Name);
+            return this.Name == null ? -1 : this.Name.CompareTo(other.Name);
         }
 
         public bool Equals(Sku other)
         {
             if (other == null) return false;
-            if (this.Name.Equals(other.Name))
+            if (this.Name == null && other.Name == null || this.Name != null && this.Name.Equals(other.Name))
             {
-                if (this.Family.Equals(other.Family))
+                if (this.Family == null && other.Family == null || this.Family != null && this.Family.Equals(other.Family))
                 {
-                    if (this.Size.Equals(other.Size))
+                    if (this.Size == null && other.Size == null || this.Size != null && this.Size.Equals(other.Size))
                     {
-                        if (this.Tier.Equals(other.Tier))
+                        if (this.Tier == null && other.Tier == null || this.Tier != null && this.Tier.Equals(other.Tier))
                         {
-                            if (this.Capacity == null && other.Capacity == null) return true;
-                            if (this.Capacity == null || other.Capacity == null) return false;
-                            return this.Capacity.Equals(other.Capacity);
+                            if (this.Capacity == null && other.Capacity == null || this.Capacity != null && this.Capacity.Equals(other.Capacity))
+                            {
+                                return true;
+                            }
                         }
                     }
                 }
