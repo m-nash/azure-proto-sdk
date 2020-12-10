55f6ce578d3b7af300ced5ed1414f369bb29d264
bquantump <53361486+bquantump@users.noreply.github.com>
2020-12-10 12:49:25 -0800

    WIP: basic arm test

--- ClientTests/ReflectionTests.cs
+++ ClientTests/ReflectionTests.cs
@@ -5,20 +5,26 @@ using azure_proto_compute;
 using Azure.Identity;
 using System;
 using Azure.ResourceManager.Resources.Models;
-using Azure.ResourceManager.Compute.Models;
+using System.Collections.Generic;
+
 namespace azure_proto_core_test
 {
-    public class ReflectionTest
+    public class ResourceListOperationsTest
     {
         [TestCase]
-        public void TestVm()
+        public void TestArmResonseArmResource()
+        {
+             TestListActivator<ArmResourceOperations, ArmResource>();
+        }
+        private static void TestListActivator<T, U>()
         {
             var testMethod = typeof(azure_proto_core.ResourceListOperations).GetMethod("CreateResourceConvertor", BindingFlags.Static | BindingFlags.NonPublic);
-            var asGeneric = testMethod.MakeGenericMethod(new System.Type[] {typeof(XVirtualMachine), typeof(PhVirtualMachine)});
+            var asGeneric = testMethod.MakeGenericMethod(new System.Type[] {typeof(T), typeof(U)});
             var context = new ArmClientContext(new Uri("https://management.azure.com"), new DefaultAzureCredential());
-            var function =  (Func<GenericResourceExpanded, XVirtualMachine>) asGeneric.Invoke(null, new object[] {context, new ArmClientOptions()} );
+            var function =  (Func<GenericResourceExpanded, T>) asGeneric.Invoke(null, new object[] {context, new ArmClientOptions()} );
             var resource = new GenericResourceExpanded();
             resource.Location = "East US";
+            resource.Tags =  new Dictionary<string, string>{ {"tag1", "value1"} };
             function.DynamicInvoke(new object [] {resource});
         }
     }
