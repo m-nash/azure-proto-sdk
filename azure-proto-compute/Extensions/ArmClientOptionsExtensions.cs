0da502fd145f97a1a1bcd2057ea86dee597badfa
nisha-bhatia <Nisha.Bhatia@microsoft.com>
2021-01-11 16:45:34 -0800

    Update ArmClientOptionsExtensions.cs

--- azure-proto-compute/Extensions/ArmClientOptionsExtensions.cs
+++ azure-proto-compute/Extensions/ArmClientOptionsExtensions.cs
@@ -2,8 +2,16 @@
 
 namespace azure_proto_compute.Extensions
 {
+    /// <summary>
+    /// A class representing an AzureResourceManagerClientOptionsExtensions along with the instance operations that can be performed on it.
+    /// </summary>
     public static class AzureResourceManagerClientOptionsExtensions
     {
+        /// <summary>
+        /// Adds a method to AzureResourceManagerClientOptions called ComputeRestVersions which returns all the versions to all resources inside the compute resource provider.
+        /// </summary>
+        /// <param name="AzureResourceManagerClientOptions"> The client parameters to use in these operations. </param>
+        /// <returns> Returns a response with the <see cref="ComputeRestApiVersions"/> operation for this resource. </returns>
         public static ComputeRestApiVersions ComputeRestVersions(this AzureResourceManagerClientOptions AzureResourceManagerClientOptions)
         {
             return AzureResourceManagerClientOptions.GetOverrideObject<ComputeRestApiVersions>(() => new ComputeRestApiVersions()) as ComputeRestApiVersions;
