fb54e5cc40edb28a5564e31b8b82ea97ecebabba
Allen Zhang <azhang@microsoft.com>
2021-01-14 10:36:46 -0800

    Xmldoc update for network extensions

--- azure-proto-network/Extensions/ResourceGroupExtensions.cs
+++ azure-proto-network/Extensions/ResourceGroupExtensions.cs
@@ -1,20 +1,44 @@
-﻿using Azure.ResourceManager.Core;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.ResourceManager.Core;
 
 namespace azure_proto_network
 {
+    /// <summary>
+    /// A class to add extension methods to Resource Group.
+    /// </summary>
     public static class ResourceGroupExtensions
     {
         #region Virtual Network Operations
+
+        /// <summary>
+        /// Gets a <see cref="VirtualNetwork"/> for a given resource under a <see cref="ResourceGroup"/>. 
+        /// </summary>
+        /// <param name="resourceGroup"> The <see cref="ResourceGroup" /> instance the method will execute against. </param>
+        /// <param name="virtualNetwork"> The <see cref="VirtualNetworkData" /> data model. </param>
+        /// <returns></returns>
         public static VirtualNetwork VirtualNetwork(this ResourceGroup resourceGroup, VirtualNetworkData virtualNetwork)
         {
             return new VirtualNetwork(resourceGroup.ClientOptions, virtualNetwork);
         }
 
+        /// <summary>
+        /// Gets a <see cref="VirtualNetworkOperations"/> for a given resource id under a <see cref="ResourceGroup"/>. 
+        /// </summary>
+        /// <param name="resourceGroup"> The <see cref="ResourceGroup" /> instance the method will execute against. </param>
+        /// <param name="virtualNetwork"> The resource id of <see cref="VirtualNetwork" /> data model. </param>
+        /// <returns></returns>
         public static VirtualNetworkOperations VirtualNetwork(this ResourceGroupOperations resourceGroup, string virtualNetwork)
         {
             return new VirtualNetworkOperations(resourceGroup.ClientOptions, new ResourceIdentifier($"{resourceGroup.Id}/providers/Microsoft.Network/virtualNetworks/{virtualNetwork}"));
         }
 
+        /// <summary>
+        /// Gets a <see cref="VirtualNetworkContainer"/> under a <see cref="ResourceGroup"/>. 
+        /// </summary>
+        /// <param name="resourceGroup"> The <see cref="ResourceGroup" /> instance the method will execute against. </param>
+        /// <returns></returns>
         public static VirtualNetworkContainer VirtualNetworks(this ResourceGroup resourceGroup)
         {
             return new VirtualNetworkContainer(resourceGroup.ClientOptions, resourceGroup.Data);
@@ -24,9 +48,11 @@ namespace azure_proto_network
         {
             return new VirtualNetworkContainer(resourceGroup.ClientOptions, resourceGroup.Id);
         }
+
         #endregion
 
         #region Public IP Address Operations
+
         public static PublicIpAddress PublicIpAddress(this ResourceGroupOperations resourceGroup, PublicIPAddressData publicIpAddress)
         {
             return new PublicIpAddress(resourceGroup.ClientOptions, publicIpAddress);
@@ -46,9 +72,11 @@ namespace azure_proto_network
         {
             return new PublicIpAddressContainer(resourceGroup.ClientOptions, resourceGroup.Id);
         }
+
         #endregion
 
         #region Network Interface (NIC) operations
+
         public static NetworkInterface NetworkInterface(this ResourceGroupOperations resourceGroup, NetworkInterfaceData networkInterface)
         {
             return new NetworkInterface(resourceGroup.ClientOptions, networkInterface);
@@ -68,9 +96,11 @@ namespace azure_proto_network
         {
             return new NetworkInterfaceContainer(resourceGroup.ClientOptions, resourceGroup.Id);
         }
+
         #endregion
 
         #region Network Security Group operations
+
         /// <summary>
         /// Create an NSG with the given open TCP ports
         /// </summary>
@@ -95,6 +125,7 @@ namespace azure_proto_network
         {
             return new NetworkSecurityGroupContainer(resourceGroup.ClientOptions, resourceGroup.Id);
         }
+
         #endregion
     }
 }
