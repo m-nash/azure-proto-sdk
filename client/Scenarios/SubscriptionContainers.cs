0f75e9329b492a41c28d71a7f4bc7cdc2bb3266c
bquantump <53361486+bquantump@users.noreply.github.com>
2021-01-15 16:32:09 -0800

    Wip: updates

--- client/Scenarios/SubscriptionContainers.cs
+++ client/Scenarios/SubscriptionContainers.cs
@@ -1,6 +1,7 @@
 using azure_proto_compute;
 using Azure.ResourceManager.Core;
 using System;
+using System.Collections.Generic;
 
 namespace client
 {
@@ -9,20 +10,41 @@ namespace client
         public override void Execute()
         {
             var sandboxId = "db1ab6f0-4769-4b27-930e-01e2ef9c123c";
-            var client = new AzureResourceManagerClient(); 
+            var client = new AzureResourceManagerClient();
             var subscriptionContainer = client.Subscription(sandboxId);
             var rg = subscriptionContainer.ResourceGroup("TestRg");
             var vm = rg.VirtualMachine("A-vm");
-            
-            ResourceIdentifier currentRg = vm.Id;
 
+            ResourceIdentifier currentRi = vm.Id; // currentRi must be base type since .parent can be any child if just traversing the tree below
+            // Walk up tree
             do
             {
-                Console.WriteLine(currentRg.Type);
-                Console.WriteLine(currentRg.Name);
-                Console.WriteLine(currentRg.Subscription);
-                currentRg = currentRg.Parent;
-            }while(!currentRg.IsRoot());
+                //Try{
+                    //ResourceGroupIdentifier z = id.convert<ResourceGroupIdentifier>(); // do something with name, sub, and rg
+                    //RgIdentiferAndNameFunc(z);// do something with name, sub, and rg
+                //}
+                currentRi = currentRi.Parent; // Because setting parent here, needs to be base type  
+            } while (!currentRi.IsRoot());
+
+            var identifiers = new List<ResourceIdentifier>();
+
+            var identifier = vm.Id;
+            while (!identifier.IsRoot())
+            {
+                identifiers.Add(identifier);
+                identifier = identifier.Parent;
+            }
+
+            foreach(var id in identifiers)
+            {
+                //do something with RG and Name and Subscription (this would require cast)
+                //Try
+                //{
+                    //ResourceGroupIdentifier z = id.convert<ResourceGroupIdentifier>();
+                    //RgIdentiferFunc(z);
+                //}
+            }
+
 
         }
     }
