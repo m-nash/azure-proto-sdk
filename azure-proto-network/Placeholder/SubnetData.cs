12bb5252559170894c451ef367b66fdf4f2b6088
bquantump <53361486+bquantump@users.noreply.github.com>
2021-01-15 17:15:59 -0800

    WIP: Updates

--- azure-proto-network/Placeholder/SubnetData.cs
+++ azure-proto-network/Placeholder/SubnetData.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 namespace azure_proto_network
 {
     /// <summary>
-    /// TODO: Subnet is a proxy resource, not a TrackedResource - need to adapt to using Resource as the base class
+    /// A class representing the subnet data model.
     /// </summary>
     public class SubnetData : ProxyResource<Azure.ResourceManager.Network.Models.Subnet>
     {
@@ -13,65 +13,106 @@ namespace azure_proto_network
         {
         }
 
+        /// <summary>
+        /// Gets the subnet id.
+        /// </summary>
         public override string Name => Model.Name;
+         /// <summary> The provisioning state of the subnet resource. </summary>
         public ProvisioningState? ProvisioningState => Model.ProvisioningState;
+
+        /// <summary>  
+        /// A read-only string identifying the intention of use for this subnet based on
+        /// delegations and other user-defined properties.
+        /// </summary>
         public string Purpose => Model.Purpose;
+        
+        /// <summary> An array of references to the delegations on the subnet. </summary>
         public IList<Delegation> Delegations
         {
             get => Model.Delegations;
             set => Model.Delegations = value;
         }
+
+        /// <summary>An array of references to services injecting into this subnet. </summary>
         public IList<ServiceAssociationLink> ServiceAssociationLinks => Model.ServiceAssociationLinks;
+
+        /// <summary>An array of references to the external resources using subnet. </summary>
         public IList<ResourceNavigationLink> ResourceNavigationLinks => Model.ResourceNavigationLinks;
+        
+        /// <summary> Array of IpAllocation which reference this subnet. </summary>
         public IList<SubResource> IpAllocations
         {
             get => Model.IpAllocations;
             set => Model.IpAllocations = value;
         }
+        /// <summary>  Array of IP configuration profiles which reference this subnet. </summary>
         public IList<IPConfigurationProfile> IpConfigurationProfiles => Model.IpConfigurationProfiles;
+
+        /// <summary> An array of references to the network interface IP configurations using subnet. </summary>
         public IList<IPConfiguration> IpConfigurations => Model.IpConfigurations;
+
+        /// <summary> Enable or Disable apply network policies on private end point in the subnet. </summary>
         public string PrivateEndpointNetworkPolicies
         {
             get => Model.PrivateEndpointNetworkPolicies;
             set => Model.PrivateEndpointNetworkPolicies = value;
         }
+
+        /// <summary> An array of references to private endpoints. </summary>
         public IList<PrivateEndpoint> PrivateEndpoints => Model.PrivateEndpoints;
+
+        /// <summary> An array of service endpoints. </summary>
         public IList<ServiceEndpointPropertiesFormat> ServiceEndpoints
         {
             get => Model.ServiceEndpoints;
             set => Model.ServiceEndpoints = value;
         }
+        /// <summary> Nat gateway associated with this subnet. </summary>
         public SubResource NatGateway
         {
             get => Model.NatGateway;
             set => Model.NatGateway = value;
         }
+
+        /// <summary> The reference to the RouteTable resource. </summary>
         public RouteTable RouteTable
         {
             get => Model.RouteTable;
             set => Model.RouteTable = value;
         }
+
+        /// <summary> The reference to the NetworkSecurityGroup resource. </summary>
         public Azure.ResourceManager.Network.Models.NetworkSecurityGroup NetworkSecurityGroup
         {
             get => Model.NetworkSecurityGroup;
             set => Model.NetworkSecurityGroup = value;
         }
+
+        /// <summary> List of address prefixes for the subnet. </summary>
         public IList<string> AddressPrefixes
         {
             get => Model.AddressPrefixes;
             set => Model.AddressPrefixes = value;
         }
+
+        /// <summary> The address prefix for the subnet. </summary>
         public string AddressPrefix
         {
             get => Model.AddressPrefix;
             set => Model.AddressPrefix = value;
         }
+
+        /// <summary>  A unique read-only string that changes whenever the resource is updated. </summary>
         public string Etag => Model.Etag;
+
+        /// <summary>  An array of service endpoint policies. </summary>
         public IList<ServiceEndpointPolicy> ServiceEndpointPolicies
         {
             get => Model.ServiceEndpointPolicies;
             set => Model.ServiceEndpointPolicies = value;
         }
+
+        /// <summary> Enable or Disable apply network policies on private link service in the subnet. </summary>
         public string PrivateLinkServiceNetworkPolicies
         {
             get => Model.PrivateLinkServiceNetworkPolicies;
--- azure-proto-network/Subnet.cs
+++ azure-proto-network/Subnet.cs
@@ -2,6 +2,9 @@
 
 namespace azure_proto_network
 {
+    /// <summary>
+    /// A class representing a subnet along with the instance operations that can be performed on it.
+    /// </summary>
     public class Subnet : SubnetOperations
     {
         public Subnet(AzureResourceManagerClientOptions SubnetOptions, SubnetData resource)
