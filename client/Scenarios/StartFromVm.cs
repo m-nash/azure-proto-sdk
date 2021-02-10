39d3eb1ae5f4548a378e86fc12337126cffba36b
nisha-bhatia <Nisha.Bhatia@microsoft.com>
2021-02-09 16:50:42 -0800

    Update StartFromVm.cs

--- client/Scenarios/StartFromVm.cs
+++ client/Scenarios/StartFromVm.cs
@@ -11,10 +11,6 @@ namespace client
             var createVm = new CreateSingleVmExample(Context);
             createVm.Execute();
             var client = new AzureResourceManagerClient();
-            //var resourceId = new ResourceIdentifier("sub/rg/vm");
-            //var vmOps = client.GetSubscriptionOperations(resourceId.Subscription).GetResourceGroupOperations(resourceId.ResourceGroup).GetVirtualMachineOperations(resourceId.Name);
-            //var vmOps1 = client.GetVirtualMachineOperations(resourceId);
-            //var vmOps1 = VirtualMachineOperations(resourceId, client);
             //retrieve from lowest level, doesn't give ability to walk up and down the container structure
             var vmOp = client.GetResourceOperations<VirtualMachineOperations>(Context.SubscriptionId, Context.RgName, Context.VmName);
             var vm = vmOp.Get().Value.Data;
