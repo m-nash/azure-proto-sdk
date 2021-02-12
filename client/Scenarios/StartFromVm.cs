1c622ac8bab5af12902c177470eb4a2bccee4bde
nisha-bhatia <Nisha.Bhatia@microsoft.com>
2021-02-12 11:11:43 -0800

    restore

--- client/Scenarios/StartFromVm.cs
+++ client/Scenarios/StartFromVm.cs
@@ -4,23 +4,22 @@ using System;
 
 namespace client
 {
-    class StartFromVm : Scenario
+    class StartStopVm : Scenario
     {
         public override void Execute()
         {
             var createVm = new CreateSingleVmExample(Context);
             createVm.Execute();
+
             var client = new AzureResourceManagerClient();
-
-            //retrieve from lowest level, doesn't give ability to walk up and down the container structure
-            var vmOp = client.GetResourceOperations<VirtualMachineOperations>(Context.SubscriptionId, Context.RgName, Context.VmName);
-            var vm = vmOp.Get().Value.Data;
-            Console.WriteLine($"Found VM {vm.Id}");
-
-            //retrieve from lowest level inside management package gives ability to walk up and down
-            var rg = client.GetResourceGroupOperations(Context.SubscriptionId, Context.RgName);
-            var vm2 = rg.GetVirtualMachineOperations(Context.VmName).Get().Value.Data;
-            Console.WriteLine($"Found VM {vm2.Id}");
+            var subscription = client.GetSubscriptionOperations(Context.SubscriptionId);
+            var resourceGroup = subscription.GetResourceGroupOperations(Context.RgName);
+            var vm = resourceGroup.GetVirtualMachineOperations(Context.VmName);
+            Console.WriteLine($"Found VM {Context.VmName}");
+            Console.WriteLine("--------Stopping VM--------");
+            vm.PowerOff();
+            Console.WriteLine("--------Starting VM--------");
+            vm.PowerOn();
         }
     }
 }
