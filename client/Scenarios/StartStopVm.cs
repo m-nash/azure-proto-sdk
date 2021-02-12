6b3ed99ea2c7a494ada3d87952ff4f821c8797f5
nisha-bhatia <Nisha.Bhatia@microsoft.com>
2021-02-12 11:14:42 -0800

    restore

--- client/Scenarios/StartStopVm.cs
+++ client/Scenarios/StartStopVm.cs
@@ -8,9 +8,12 @@ namespace client
     {
         public override void Execute()
         {
+            var createVm = new CreateSingleVmExample(Context);
+            createVm.Execute();
+
             var client = new AzureResourceManagerClient();
             var subscription = client.GetSubscriptionOperations(Context.SubscriptionId);
-            var resourceGroup = subscription.GetResourceGroupOperations("nbhatia-test");
+            var resourceGroup = subscription.GetResourceGroupOperations(Context.RgName);
             var vm = resourceGroup.GetVirtualMachineOperations(Context.VmName);
             Console.WriteLine($"Found VM {Context.VmName}");
             Console.WriteLine("--------Stopping VM--------");
